e40cd75db34a67ea02d509d4a90183da4c17cc75
baoyao <111446186+vrybadplayer@users.noreply.github.com>
2023-08-03 15:14:04 +0800

    File Creation
    Created OOP File for GitHub

---
+++ OOP_Assignment/src/oop_assignment/OOP_Assignment.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package oop_assignment;
+
+/**
+ *
+ * @author ASUS
+ */
+public class OOP_Assignment {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    
+}
