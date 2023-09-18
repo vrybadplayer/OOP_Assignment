c0f365e455f0afd446346e64093b2337280d5164
baoyao <111446186+vrybadplayer@users.noreply.github.com>
2023-09-18 23:27:56 +0800

    Finished Testing
    Reset to how it used to be

--- OOP_Assignment/src/oop_assignment/Driver.java
+++ OOP_Assignment/src/oop_assignment/Driver.java
@@ -7,9 +7,9 @@ import java.awt.event.KeyEvent;
 import java.lang.annotation.Repeatable;
 import java.util.*;
 import java.util.Scanner;
-//GitHub Pull Test 2
+
 public class Driver {
-    //GitHub Push Test 2
+    
     private static boolean proceed = false;
     private static boolean member = false;
     private static double balance = 0;
